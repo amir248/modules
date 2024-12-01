5f4d4a0ad289aecf2f3d4166f248c3204fcea89e
chikchilcy <chikchicly@gmail.com>
2024-12-01 15:54:13 +0500

    second

--- modules/second.js
+++ modules/second.js
@@ -1,7 +1,7 @@
 function second(){
     let second=document.createElement('div');
     second.setAttribute('id',"second");
-    document.querySelector('main').append(second);
+    document.querySelector('article').append(second);
     document.querySelector('#second').innerHTML=`it\'s Second Function!`;
 }
 export { second };
