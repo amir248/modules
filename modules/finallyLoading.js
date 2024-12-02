34310beb2309742d41060a7fbb71b1ad89954d37
chikchilcy <chikchicly@gmail.com>
2024-12-02 07:39:05 +0500

    v

--- modules/finallyLoading.js
+++ modules/finallyLoading.js
@@ -2,6 +2,9 @@ function finallyLoadingThisPage(){
     new Promise((resolve)=>{
         function oK(){
             console.log("finally Loading This Page");
+            let finallyL=document.createElement('h3');
+            finallyL.innerHTML='Finally Loading after DomContentLoading';
+            document.querySelector('article').append(finallyL);
         }
         resolve(oK());
     });
