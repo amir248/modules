41d053e85ef439c347b9271d7c8ce9bd93649d93
chikchilcy <chikchicly@gmail.com>
2024-12-02 07:31:26 +0500

    finally!

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ sayHi('John'); // Hello, John!
 sayBye('John'); // Bye, John!
 
 async function main() {
-    const finallyLoading= await import("modules/finallyLoading.js");
-    // window.addEventListener('DOMContentLoaded',finallyLoading.finallyLoadingThisPage());
-    finallyLoading.finallyLoadingThisPage();
+    const finallyLoading= await import("https://amir248.github.io/modules/modules/finallyLoading.js");
+    window.addEventListener('DOMContentLoaded',finallyLoading.finallyLoadingThisPage());
+    // finallyLoading.finallyLoadingThisPage();
     const myModule = await import("https://amir248.github.io/modules/modules/first.js");
     // use myModule
     myModule.first();
