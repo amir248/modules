460f5ff55088bad658e6b45d9fc7982d70b0633b
chikchilcy <chikchicly@gmail.com>
2024-12-02 07:49:07 +0500

    hr

--- main.js
+++ main.js
@@ -7,20 +7,21 @@ import {sayHi, sayBye} from 'https://amir248.github.io/modules/modules/say.js';
 sayHi('John'); // Hello, John!
 sayBye('John'); // Bye, John!
 
-async function main() {
-    const finallyLoading= await import("https://amir248.github.io/modules/modules/finallyLoading.js");
-    window.addEventListener('DOMContentLoaded',finallyLoading.finallyLoadingThisPage());
-    // finallyLoading.finallyLoadingThisPage();
+async function main(){
     const myModule = await import("https://amir248.github.io/modules/modules/first.js");
-    // use myModule
     myModule.first();
-    const example=await import("https://amir248.github.io/modules/modules/example.js");
-    example.example();
-    
     const secondModule=await import("https://amir248.github.io/modules/modules/second.js");
     secondModule.second();
     await sayHi("AmirDzhan!");
     await sayBye('Communist!');
+    const finallyLoading= await import("https://amir248.github.io/modules/modules/finallyLoading.js");
+    window.addEventListener('DOMContentLoaded',finallyLoading.finallyLoadingThisPage());
+    // finallyLoading.finallyLoadingThisPage();
+    // use myModule
+    const example=await import("https://amir248.github.io/modules/modules/example.js");
+    example.example();
+    
+    
 }
 
 main();
